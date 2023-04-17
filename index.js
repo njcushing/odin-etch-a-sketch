aa7b86ffe1ab678cd08ec3e6c16882cdd5340af5
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-17 22:58:04 +0100

    Made div square backgrounds turn black when the mouse enters them

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-let gridWidth = 16;
-let gridHeight = 16;
+let gridWidth = 960;
+let gridHeight = 960;
+let divsHorizontal = 16;
+let divsVertical = 16;
 let divWidth = 10;
 let divHeight = 10;
 let divSpacing = 2;
 let divBorderThickness = 2;
-let divGridWidth = ((gridWidth * divWidth) +
-                    (gridWidth * divBorderThickness) +
-                    Math.max(0, ((gridWidth - 1) * (divSpacing * 2))) +
+let divGridWidth = ((divsHorizontal * divWidth) +
+                    (divsHorizontal * divBorderThickness) +
+                    Math.max(0, ((divsHorizontal - 1) * (divSpacing * 2))) +
                     (divSpacing * 2));
 
 let container = document.querySelector('.container');
@@ -17,11 +19,10 @@ let divGrid = document.createElement('div');
 divGrid.style.display = 'flex';
 divGrid.style.justifyContent = 'center';
 divGrid.style.flexWrap = 'wrap';
-divGrid.style.border = 'solid 1px black';
 divGrid.style.width = `${divGridWidth}px`;
 container.appendChild(divGrid);
 
-for(let i = 0; i < (gridWidth * gridHeight); i++){
+for(let i = 0; i < (divsHorizontal * divsVertical); i++){
     let square = document.createElement('div');
 
     square.style.border = 'solid 1px black';
@@ -33,5 +34,12 @@ for(let i = 0; i < (gridWidth * gridHeight); i++){
     square.style.flexShrink = `0`;
     square.style.flexGrow = `0`;
 
+    square.addEventListener('mouseenter', cursorEnteredSquare);
+
     divGrid.appendChild(square);
+}
+
+function cursorEnteredSquare(e) {
+    e.target.style.backgroundColor = 'black';
+
 }
