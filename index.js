81d7c27ae36d6c0ba403586ad3c2e9702465f298
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-17 23:20:06 +0100

    Made working reset button that clears div square colour

--- index.js
+++ index.js
@@ -2,30 +2,45 @@ let gridWidth = 960;
 let gridHeight = 960;
 let divsHorizontal = 16;
 let divsVertical = 16;
-let divWidth = 10;
-let divHeight = 10;
 let divSpacing = 2;
-let divBorderThickness = 2;
-let divGridWidth = ((divsHorizontal * divWidth) +
-                    (divsHorizontal * divBorderThickness) +
-                    Math.max(0, ((divsHorizontal - 1) * (divSpacing * 2))) +
-                    (divSpacing * 2));
+let divBorderThickness = 1;
+
+let divWidthFull = Math.floor((gridWidth - (divSpacing * 2)) / (divsHorizontal));
+let divWidth = divWidthFull - (divBorderThickness * 2) - (divSpacing * 2);
+let divHeightFull = Math.floor((gridHeight - (divSpacing * 2)) / (divsVertical));;
+let divHeight = divHeightFull - (divBorderThickness * 2) - (divSpacing * 2);
 
 let container = document.querySelector('.container');
 container.style.display = 'flex';
+container.style.alignItems = 'center';
+container.style.flexDirection = 'column';
+
+let buttonContainer = document.createElement('div');
+container.style.display = 'flex';
 container.style.justifyContent = 'center';
+container.style.gap = '10px';
+
+let resetButton = document.createElement('button');
+resetButton.textContent = "Reset";
+resetButton.addEventListener('click', reset);
+buttonContainer.appendChild(resetButton);
+
+container.appendChild(buttonContainer);
 
 let divGrid = document.createElement('div');
 divGrid.style.display = 'flex';
 divGrid.style.justifyContent = 'center';
 divGrid.style.flexWrap = 'wrap';
-divGrid.style.width = `${divGridWidth}px`;
+divGrid.style.flexShrink = '0';
+divGrid.style.flexGrow = '0';
+divGrid.style.width = `${gridWidth}px`;
+divGrid.style.height = `${gridHeight}px`;
 container.appendChild(divGrid);
 
 for(let i = 0; i < (divsHorizontal * divsVertical); i++){
     let square = document.createElement('div');
 
-    square.style.border = 'solid 1px black';
+    square.style.border = `solid ${divBorderThickness}px black`;
 
     square.style.minWidth = `${divWidth}px`;
     square.style.minHeight = `${divHeight}px`;
@@ -34,12 +49,17 @@ for(let i = 0; i < (divsHorizontal * divsVertical); i++){
     square.style.flexShrink = `0`;
     square.style.flexGrow = `0`;
 
+    square.classList.add('div-square');
     square.addEventListener('mouseenter', cursorEnteredSquare);
 
     divGrid.appendChild(square);
 }
 
+function reset() {
+    let divSquares = document.querySelectorAll('.div-square');
+    divSquares.forEach(divSquare => { divSquare.style.backgroundColor = 'white'; })
+}
+
 function cursorEnteredSquare(e) {
     e.target.style.backgroundColor = 'black';
-
 }
