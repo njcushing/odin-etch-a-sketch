70f435eb4e3d314b6685e89a9465d836c422d059
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-17 22:41:03 +0100

    Made div square into 16x16 grid and aligned it in centre of screen

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ let gridHeight = 16;
 let divWidth = 10;
 let divHeight = 10;
 let divSpacing = 2;
+let divBorderThickness = 2;
+let divGridWidth = ((gridWidth * divWidth) +
+                    (gridWidth * divBorderThickness) +
+                    Math.max(0, ((gridWidth - 1) * (divSpacing * 2))) +
+                    (divSpacing * 2));
 
-let container = document.querySelector(".container");
+let container = document.querySelector('.container');
 container.style.display = 'flex';
+container.style.justifyContent = 'center';
+
+let divGrid = document.createElement('div');
+divGrid.style.display = 'flex';
+divGrid.style.justifyContent = 'center';
+divGrid.style.flexWrap = 'wrap';
+divGrid.style.border = 'solid 1px black';
+divGrid.style.width = `${divGridWidth}px`;
+container.appendChild(divGrid);
 
 for(let i = 0; i < (gridWidth * gridHeight); i++){
     let square = document.createElement('div');
@@ -16,5 +30,8 @@ for(let i = 0; i < (gridWidth * gridHeight); i++){
     square.style.minHeight = `${divHeight}px`;
     square.style.margin = `${divSpacing}px`;
 
-    container.appendChild(square);
+    square.style.flexShrink = `0`;
+    square.style.flexGrow = `0`;
+
+    divGrid.appendChild(square);
 }
