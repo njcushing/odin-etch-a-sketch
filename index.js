f002e5613b74cdaf0ad27af7bb45a262496f845e
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-18 20:33:16 +0100

    Add grid resize button and various colour modes

--- index.js
+++ index.js
@@ -2,28 +2,48 @@ let gridWidth = 960;
 let gridHeight = 960;
 let divsHorizontal = 16;
 let divsVertical = 16;
-let divSpacing = 2;
+let divSpacing = 0;
 let divBorderThickness = 1;
 
-let divWidthFull = Math.floor((gridWidth - (divSpacing * 2)) / (divsHorizontal));
-let divWidth = divWidthFull - (divBorderThickness * 2) - (divSpacing * 2);
-let divHeightFull = Math.floor((gridHeight - (divSpacing * 2)) / (divsVertical));;
-let divHeight = divHeightFull - (divBorderThickness * 2) - (divSpacing * 2);
+let divWidthFull;
+let divWidth;
+let divHeightFull;
+let divHeight;
+
+let colourMode = 0;
+let squaresColoured = 0;
+
+calculateDivSquareSize();
 
 let container = document.querySelector('.container');
 container.style.display = 'flex';
 container.style.alignItems = 'center';
 container.style.flexDirection = 'column';
-
-let buttonContainer = document.createElement('div');
-container.style.display = 'flex';
-container.style.justifyContent = 'center';
 container.style.gap = '10px';
 
+let buttonContainer = document.createElement('div');
+buttonContainer.style.display = 'flex';
+buttonContainer.style.justifyContent = 'center';
+buttonContainer.style.gap = '10px';
+
 let resetButton = document.createElement('button');
-resetButton.textContent = "Reset";
+resetButton.textContent = 'Reset';
+resetButton.style.minWidth = '240px';
+resetButton.style.padding = '6px 20px';
 resetButton.addEventListener('click', reset);
 buttonContainer.appendChild(resetButton);
+
+let changeSizeButton = document.createElement('button');
+changeSizeButton.textContent = 'Change Grid Size';
+changeSizeButton.style.minWidth = '240px';
+changeSizeButton.addEventListener('click', changeGridSize);
+buttonContainer.appendChild(changeSizeButton);
+
+let colourModeButton = document.createElement('button');
+colourModeButton.textContent = 'Colour Mode: White to Black';
+colourModeButton.style.minWidth = '240px';
+colourModeButton.addEventListener('click', changeColourMode);
+buttonContainer.appendChild(colourModeButton);
 
 container.appendChild(buttonContainer);
 
@@ -37,29 +57,102 @@ divGrid.style.width = `${gridWidth}px`;
 divGrid.style.height = `${gridHeight}px`;
 container.appendChild(divGrid);
 
-for(let i = 0; i < (divsHorizontal * divsVertical); i++){
-    let square = document.createElement('div');
+createGrid();
 
-    square.style.border = `solid ${divBorderThickness}px black`;
+function calculateDivSquareSize() {
+    divWidthFull = Math.floor((gridWidth - (divSpacing * 2)) / (divsHorizontal));
+    divWidth = divWidthFull - (divBorderThickness * 2) - (divSpacing * 2);
+    divHeightFull = Math.floor((gridHeight - (divSpacing * 2)) / (divsVertical));;
+    divHeight = divHeightFull - (divBorderThickness * 2) - (divSpacing * 2);
+}
 
-    square.style.minWidth = `${divWidth}px`;
-    square.style.minHeight = `${divHeight}px`;
-    square.style.margin = `${divSpacing}px`;
+function createGrid() {
+    for(let i = 0; i < (divsHorizontal * divsVertical); i++){
+        let square = document.createElement('div');
 
-    square.style.flexShrink = `0`;
-    square.style.flexGrow = `0`;
+        square.style.border = `solid ${divBorderThickness}px black`;
 
-    square.classList.add('div-square');
-    square.addEventListener('mouseenter', cursorEnteredSquare);
+        square.style.minWidth = `${divWidth}px`;
+        square.style.minHeight = `${divHeight}px`;
+        square.style.margin = `${divSpacing}px`;
 
-    divGrid.appendChild(square);
+        square.style.flexShrink = `0`;
+        square.style.flexGrow = `0`;
+
+        square.classList.add('div-square');
+        square.addEventListener('mouseenter', cursorEnteredSquare);
+
+        divGrid.appendChild(square);
+    }
 }
 
 function reset() {
     let divSquares = document.querySelectorAll('.div-square');
     divSquares.forEach(divSquare => { divSquare.style.backgroundColor = 'white'; })
+    squaresColoured = 0;
+}
+
+function changeGridSize() {
+    let width, height;
+    while(true){
+        width = prompt("Type the number of squares to have horizontally (limit of 100):");
+        if(width == null){ return; }
+        width = parseInt(width);
+        if(isNaN(width)){ console.log("here"); continue; }
+        if(width < 1 || width > 100){ continue; }
+        break;
+    }
+    while(true){
+        height = prompt("Type the number of squares to have vertically (limit of 100):");
+        if(height == null){ return; }
+        height = parseInt(height);
+        if(isNaN(height)){ console.log("here"); continue; }
+        if(height < 1 || height > 100){ continue; }
+        break;
+    }
+    divsHorizontal = width;
+    divsVertical = height;
+
+    let divSquares = document.querySelectorAll('.div-square');
+    divSquares.forEach(divSquare => { divGrid.removeChild(divSquare); })
+
+    calculateDivSquareSize();
+
+    let newWidth = divWidthFull * divsHorizontal;
+    let newHeight = divHeightFull * divsVertical;
+    divGrid.style.width = `${newWidth}px`;
+    divGrid.style.height = `${newHeight}px`;
+
+    createGrid();
+
+    squaresColoured = 0;
+}
+
+function changeColourMode(e) {
+    colourMode = (colourMode + 1) % 3;
+    switch(colourMode){
+        case 0: e.target.textContent = 'Colour Mode: White to Black'; break;
+        case 1: e.target.textContent = 'Colour Mode: Fade to Black'; break;
+        case 2: e.target.textContent = 'Colour Mode: Random Colours'; break;
+    }
 }
 
 function cursorEnteredSquare(e) {
-    e.target.style.backgroundColor = 'black';
+    switch(colourMode){
+        case 0:
+            e.target.style.backgroundColor = 'black';
+            break;
+        case 1:
+            let fadeToBlack = 255 * (Math.max(0, 10 - squaresColoured) / 10);
+            e.target.style.backgroundColor = `rgb(${fadeToBlack}, ${fadeToBlack}, ${fadeToBlack})`;
+            break;
+        case 2:
+            e.target.style.backgroundColor = `rgb(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()})`;
+            break;
+    }
+    squaresColoured++;
+}
+
+function randomRGBValue(num) {
+    return Math.floor(Math.random() * 256);
 }
